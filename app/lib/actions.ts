e7da66cdc1c463242ce5fbe93f3ee2fc33541bc5
Isak <isak.koski96@gmail.com>
2024-05-10 17:16:58 +0200

    add form validation to creating invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,17 +7,41 @@ import { z } from "zod";
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        required_error: 'Please select a customer',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Enter a number greater than 0'}),
+    status: z.enum(['pending', 'paid'], {
+        required_error: 'Please select an invoice status',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData.entries()));
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse(Object.fromEntries(formData.entries()));
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
